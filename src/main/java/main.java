037302e029ac347254eccb020c502a1209a76f90
Phi <000214364@nmmi.edu>
2020-12-29 01:47:46 -0700

    bruh

---
+++ src/main/java/main.java
@@ -0,0 +1,13 @@
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class main
+{
+    public static void main(String args[]) throws Exception
+    {
+        JDA jda = new JDABuilder("NzkzMzkwMjc0ODIyMDEyOTM5.X-rkRQ.sYCmOM2KSASKvtcM94Bb6SiCnIQ").build();
+
+
+    }
+}
